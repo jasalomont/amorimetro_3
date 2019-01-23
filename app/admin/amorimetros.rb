1742e8f7aa6f09079bd5ef64b612651517c0ea66
Raghu Betina <raghu@firstdraft.com>
2019-01-23 17:12:28 +0000

    Whitelist columns in Amorimetro for ActiveAdmin

--- app/admin/amorimetros.rb
+++ app/admin/amorimetros.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Amorimetro do
+
+ permit_params :points, :user_id, :reason
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
